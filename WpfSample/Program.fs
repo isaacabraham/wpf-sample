674700690b9ecc5d38ac62b72ac1bc1061b3b6ce
Isaac Abraham <isaac.abraham@gmail.com>
2018-03-09 17:12:03 +0100

    Initial commit.

---
+++ WpfSample/Program.fs
@@ -0,0 +1,17 @@
+﻿open FsXaml
+open System
+open System.Windows
+
+type MainWindow = XAML<"Main.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let window = MainWindow()
+    
+    window.Button1.Click |> Event.add(fun c -> MessageBox.Show "You clicked button 1!" |> ignore)
+    window.Button2.Click |> Event.add(fun c -> MessageBox.Show "You clicked button 2!" |> ignore)    
+    
+    let app = Application()
+    app.Run(window)
