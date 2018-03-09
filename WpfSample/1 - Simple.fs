d94ec67aff22d72372235e9d8e9c7f523e972e0d
Isaac Abraham <isaac.abraham@gmail.com>
2018-03-09 17:28:57 +0100

    Simple inc / dec / reset.

--- WpfSample/1 - Simple.fs
+++ WpfSample/1 - Simple.fs
@@ -9,16 +9,12 @@ type MainWindow = XAML<"Main.xaml">
 
 let createWindow() =
     let window = MainWindow()
-    
-    window.Button1.Click
-    |> Event.add(fun c ->
-        MessageBox.Show "You clicked button 1!" |> ignore
-        window.TextBox1.Text <- "Button 1")
+    let mutable value = 0
 
-    window.Button2.Click
-    |> Event.add(fun c ->
-        sprintf "You clicked button 2! Text is '%s'." window.TextBox1.Text
-        |> MessageBox.Show 
-        |> ignore)
+    let update() = window.TextBlock1.Text <- string value
+    
+    window.Button1.Click |> Event.add(fun c -> value <- value + 1; update())
+    window.Button2.Click |> Event.add(fun c -> value <- value - 1; update())
+    window.Reset.Click |> Event.add(fun c -> value <- 0; update())
 
     window
