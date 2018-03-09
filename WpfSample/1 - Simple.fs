2d0503eeaa5ea9b2952b39a05238d4424ae1e532
Isaac Abraham <isaac.abraham@gmail.com>
2018-03-09 17:19:34 +0100

    Sample 1

---
+++ WpfSample/1 - Simple.fs
@@ -0,0 +1,24 @@
+﻿/// This sample represents a "basic" WPF application using raw event handlers etc. - essentially
+/// directly modifying the view.
+module Samples.Simple
+
+open FsXaml
+open System.Windows
+
+type MainWindow = XAML<"Main.xaml">
+
+let createWindow() =
+    let window = MainWindow()
+    
+    window.Button1.Click
+    |> Event.add(fun c ->
+        MessageBox.Show "You clicked button 1!" |> ignore
+        window.TextBox1.Text <- "Button 1")
+
+    window.Button2.Click
+    |> Event.add(fun c ->
+        sprintf "You clicked button 2! Text is '%s'." window.TextBox1.Text
+        |> MessageBox.Show 
+        |> ignore)
+
+    window
--- WpfSample/Program.fs
+++ WpfSample/Program.fs
@@ -1,17 +1,8 @@
-﻿open FsXaml
-open System
+﻿open System
 open System.Windows
-
-type MainWindow = XAML<"Main.xaml">
 
 [<STAThread>]
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    let window = MainWindow()
-    
-    window.Button1.Click |> Event.add(fun c -> MessageBox.Show "You clicked button 1!" |> ignore)
-    window.Button2.Click |> Event.add(fun c -> MessageBox.Show "You clicked button 2!" |> ignore)    
-    
+let main _ = 
     let app = Application()
-    app.Run(window)
+    Samples.Simple.createWindow() |> app.Run
